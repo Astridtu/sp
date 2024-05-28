7cf2de7cbf08ef87b4700106beb9c77f2eee4c5d
Astridtu <125680827+Astridtu@users.noreply.github.com>
2024-05-28 18:54:28 +0800

    Create race.c

---
+++ Homework4/race.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define LOOPS 100000
+int account_balance = 1000;
+
+void *deposit(void *arg) {
+    for (int i = 0; i < LOOPS; i++) {
+        account_balance += 1;
+    }
+    return NULL;
+}
+
+void *withdraw(void *arg) {
+    for (int i = 0; i < LOOPS; i++) {
+        account_balance -= 1;
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t thread1, thread2;
+
+    pthread_create(&thread1, NULL, deposit, NULL);
+    pthread_create(&thread2, NULL, withdraw, NULL);
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    printf("Final account balance: %d\n", account_balance);
+
+    return 0;
+}
